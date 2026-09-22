d1564aba1b4aa9937b579a817e2d6c3c94713177
Grady Horn <gthorn@asu.edu>
2026-09-21 20:18:49 -0700

    Initial addition of screen transition animation

---
+++ script.js
@@ -0,0 +1,8 @@
+
+beginButton.addEventListener("click", () => {
+    transition.classList.add("play-transitionl");
+    setTimeout(() => {
+        //switch to the next screen or perform any other action after the transition
+
+    }, 400);
+});
